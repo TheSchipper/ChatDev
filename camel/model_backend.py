769a10e7a97c02b277ba99b0a5a03367a162c609
TheSchipper <107776081+TheSchipper@users.noreply.github.com>
2023-12-28 06:05:31 -0800

    Revert

--- camel/model_backend.py
+++ camel/model_backend.py
@@ -30,7 +30,7 @@ except ImportError:
 
 import os
 
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY', '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')
+OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
 if 'BASE_URL' in os.environ:
     BASE_URL = os.environ['BASE_URL']
 else:
